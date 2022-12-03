d3ee67cae1468e056c32d04985b7e66ebec5cd95
Nurtimax <maksatbeknurtilek@gmail.com>
2022-12-03 03:11:16 -0800

    update project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import DefinedPages from "./layouts/DefinedPages";
 import Home from "./pages/Home";
 import List from "./pages/List";
@@ -35,19 +35,21 @@ function App() {
   }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path="/" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/:id" element={<DefinedPages />} />
-      <Route path="/home" element={<Home />} />
-      {data.trelloCardList.map((card) => (
-        <Route
-          key={card.id}
-          path={`/lists/${card.title.toLowerCase()}`}
-          element={<List {...card} />}
-        />
-      ))}
-    </Routes>
+    <HashRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/:id" element={<DefinedPages />} />
+        <Route path="/home" element={<Home />} />
+        {data.trelloCardList.map((card) => (
+          <Route
+            key={card.id}
+            path={`/lists/${card.title.toLowerCase()}`}
+            element={<List {...card} />}
+          />
+        ))}
+      </Routes>
+    </HashRouter>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -6,16 +6,11 @@ import { Provider } from "react-redux";
 import store from "./store";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { BrowserRouter, HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <ToastContainer />
-    <BrowserRouter>
-      <HashRouter basename="/">
-        <App />
-      </HashRouter>
-    </BrowserRouter>
+      <App />
   </Provider>
 );
